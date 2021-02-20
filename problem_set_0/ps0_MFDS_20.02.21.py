6e7cbd7cd3ecb72a61b256b9207e5ecab494dde2
mfdspicer <69670154+mfdspicer@users.noreply.github.com>
2021-02-20 15:19:56 +0100

    Create ps0_MFDS_20.02.21.py

---
+++ problem_set_0/ps0_MFDS_20.02.21.py
@@ -0,0 +1,11 @@
+#Write a program that does the following in order:
+#   1. Asks the user to enter a number “x”
+#   2. Asks the user to enter a number “y”
+#   3. Prints out number “x”, raised to the power “y”.
+#   4. Prints out the log (base 2) of “x”.
+
+import numpy
+x = float(input("Enter number x: "))
+y = float(input("Enter number y: "))
+print("x**y =",int(x**y))
+print("log2(x) =",int(numpy.log2(x)))
