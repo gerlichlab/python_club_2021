a8f83d0755e7a07c4d3ad9707b95f1a13389819d
mfdspicer <69670154+mfdspicer@users.noreply.github.com>
2021-02-25 10:45:18 +0100

    Update and rename ps0_MFDS_24.02.21.py to ps0_MFDS_25.02.21.py

--- problem_set_0/ps0_MFDS_25.02.21.py
+++ problem_set_0/ps0_MFDS_25.02.21.py
@@ -1,8 +1,19 @@
-#Write a program that does the following in order:
+#The assignment was to write a programme that does the following in order:
 #   1. Asks the user to enter a number “x”
 #   2. Asks the user to enter a number “y”
 #   3. Prints out number “x”, raised to the power “y”.
 #   4. Prints out the log (base 2) of “x”.
+#
+#My original solution to this problem was as follows:
+# import numpy
+# x = float(input("Enter number x: "))
+# y = float(input("Enter number y: "))
+# print("x**y =",float(x**y)
+# print("log2(x) =",float(numpy.log2(x))) 
+#
+#
+#For v2, I wanted the programme to recognise if the user fails to enter a number and, rather than produce a ValueError, ask again for a number with a medium amount of sass:
+
 
 import numpy
 while True:
