8273dfee68b1b903de53451206fda718a8592060
Federico Teloni <57997363+FedeTelo@users.noreply.github.com>
2021-02-25 12:04:47 +0100

    Update and rename FT_solution.py to 210225_FT_solution.py

--- problem_set_0/210225_FT_solution.py
+++ problem_set_0/210225_FT_solution.py
@@ -1,16 +1,12 @@
 import numpy as np
-
-print("""
-This program calculates the result of raising a number X to the power of Y and the log (base 2) of X
-Please input number X """)
+print("This program calculates the result of raising a number X to the power of Y and the log (base 2) of X")
 #Input number X
-x = float(input ())
+x = float(input("Please input number X: "))
 #Input number Y
-print ("Please input number Y")
-y = float(input ())
+y = float(input("Please input number Y: "))
 #Print out number X raised to the power of Y
 print ("The results of ", x, " to the power of ", y, " is ", x**y)
-#Print out log (base2) of X
+#Print out log (base2) of X if X => 0
 if x > 0:
     print ("The log2 of ", x, " is ", np.log2(x))
 else:
