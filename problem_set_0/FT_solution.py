5c6280af57a2f72c7c902c7a08350e4a0149e8ca
Federico Teloni <57997363+FedeTelo@users.noreply.github.com>
2021-02-25 10:48:00 +0100

    Update FT_solution.py

--- problem_set_0/FT_solution.py
+++ problem_set_0/FT_solution.py
@@ -2,12 +2,16 @@ import numpy as np
 
 print("""
 This program calculates the result of raising a number X to the power of Y and the log (base 2) of X
-Please input number X
-""")
-x = int(input ())
+Please input number X """)
+#Input number X
+x = float(input ())
+#Input number Y
 print ("Please input number Y")
-y = int(input ())
-power = pow(x, y)
-print ("The results of ", x, " to the power of ", y, " is ",power)
-log = (np.log2(x))
-print ("The log2 of ", x, " is ", float(log))
+y = float(input ())
+#Print out number X raised to the power of Y
+print ("The results of ", x, " to the power of ", y, " is ", x**y)
+#Print out log (base2) of X if X => 0
+if x > 0:
+    print ("The log2 of ", x, " is ", np.log2(x))
+else:
+    print ("The log of X <= 0 is undefined!")
