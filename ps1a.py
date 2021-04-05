3ce61115282d353997c222bf94a477e94a41278c
mfdspicer <69670154+mfdspicer@users.noreply.github.com>
2021-04-05 18:24:10 +0200

    Add files via upload

--- ps1a.py
+++ ps1a.py
@@ -1,9 +1,27 @@
+#Write a program to calculate how many months it will take you to save up enough money for a down payment. You will want your main variables to be floats, so you should cast user inputs to floats.
+
+def catching_failed_float_inputs(prompt):
+    """
+    This function checks whether the user has provided a valid number as input, and will try again if parameters are not met.
+    """
+    errormsg = "That number is not valid, please try again"
+    while True:
+        try: x = float(input(prompt))
+        except ValueError:
+            print(errormsg)
+            x = catching_failed_float_inputs(prompt)
+        break
+    if x <= 0:
+        print(errormsg)
+        x = catching_failed_float_inputs(prompt)
+    return x
+
 current_savings = 0
 r = 0.04
-annual_salary = float(input("Enter your annual salary:"))
+annual_salary = catching_failed_float_inputs("Enter your annual salary: ")
 monthly_salary = annual_salary / 12
-portion_saved = monthly_salary * float(input("Enter the portion of salary you are able to save:"))
-total_cost = float(input("Enter the cost of your dream house:"))
+portion_saved = monthly_salary * catching_failed_float_inputs("Enter the percent of your salary to save, as a decimal: ")
+total_cost = catching_failed_float_inputs("Enter the cost of your dream home: ")
 portion_down_payment = 0.25 * total_cost
 
 i = 0
