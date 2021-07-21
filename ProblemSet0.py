b5a6e4c01deb4721c652ac4a3afa2b3e9ecd0a48
emokegerocz <87698519+emokegerocz@users.noreply.github.com>
2021-07-21 17:10:28 +0200

    Add files via upload

---
+++ ProblemSet0.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 16:52:58 2021
+
+@author: emoke.gerocz
+"""
+
+"""
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”."""
+
+
+x=float(input("Please input a number for x!"))
+y=float(input("Please input another number, this will be y!"))
+print("Here comes the maigc trick!")
+""" Sorry I took some liberty to make this fun!"""
+print("x^y=", x**y)
+print ("And the log of x is...", x**0.5)
+      
+        
